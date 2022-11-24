ef4e889a4109d483ab73bf98db1302ade0d9de41
kasiabauer <bauer.kasia@gmail.com>
2022-11-24 23:00:29 +0100

    Added Donation model.

--- fundation_app/models.py
+++ fundation_app/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 INSTITUTION_TYPE = (
@@ -22,3 +23,20 @@ class Institution(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+
+class Donation(models.Model):
+    quantity = models.PositiveSmallIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution)
+    address = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=16, unique=True)
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=64)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.CharField(max_length=128)
+    user = models.ForeignKey(User, default=None)
+
+    def __str__(self):
+        return f'Donation id:{self.id}'
