48553cee460e73262c527215b228f43973ff3e03
kasiabauer <bauer.kasia@gmail.com>
2022-11-24 16:59:52 +0100

    added pytest and pytest conf files added 5 tests for 5 views for 200 response code

---
+++ fundation_app/templates/tests.py
@@ -0,0 +1,44 @@
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+# 1st test checking the system
+@pytest.mark.django_db
+def test_001_check_settings():
+    assert True
+
+
+# 1st test for LandingPageview
+def test_002_get_landing_page(client):
+    url = reverse('landing-page')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+# 1st test for Loginview
+def test_003_get_login(client):
+    url = reverse('login')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+# 1st test for Registerview
+def test_004_get_register(client):
+    url = reverse('register')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+# 1st test for AddDonationView
+def test_005_get_add_donation(client):
+    url = reverse('add-donation')
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+# 1st test for FormConfirmView
+def test_006_get_form_confirm(client):
+    url = reverse('form-confirm')
+    response = client.get(url)
+    assert response.status_code == 200
